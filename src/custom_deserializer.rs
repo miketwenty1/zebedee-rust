a341cfc523b72597bdc13b3644fef443626f79a6
Sergiu Popescu <sergiu.popescu@adinmo.com>
2024-02-13 17:22:57 +0100

    add custom deserializer for option types

--- src/custom_deserializer.rs
+++ src/custom_deserializer.rs
@@ -11,3 +11,20 @@ where
         .parse::<T>()
         .map_err(serde::de::Error::custom)
 }
+pub fn deserialize_from_m_string<'de, T, D>(deserializer: D) -> Result<Option<T>, D::Error>
+where
+    D: Deserializer<'de>,
+    T: FromStr + serde::Deserialize<'de>,
+    <T as FromStr>::Err: Display,
+{
+    match Option::<String>::deserialize(deserializer) {
+        Ok(Some(value)) => {
+            let v = value.parse::<T>().map_err(serde::de::Error::custom)?;
+
+            Ok(Some(v))
+        }
+        Ok(None) => Ok(None),
+
+        Err(e) => Err(e),
+    }
+}
