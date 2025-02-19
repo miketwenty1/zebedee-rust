175981070642ad994a804b0cef8deffbf3f662cd
Michael Tidwell <tidwell@zebedee.io>
2025-02-18 21:26:21 -0500

    fixing test

--- src/keysend/tests.rs
+++ src/keysend/tests.rs
@@ -8,11 +8,14 @@ async fn test_keysend() {
     let zbdenv: String =
         env::var("ZBD_ENV").unwrap_or_else(|_| String::from("https://api.zebedee.io"));
     let zebedee_client = ZebedeeClient::new(apikey).domain(zbdenv);
-    let tlvs = vec![TlvRecord { record_type : 123123123, value : String::from("00ABCDEF") }];
+    let tlvs = vec![TlvRecord {
+        record_type: 123123123,
+        value: String::from("00ABCDEF"),
+    }];
 
     let keysend_payload = Keysend {
         amount: String::from("1000"),
-        pubkey: String::from("03f80288f858251aed6f70142fab79dede5427a0ff4b618707bd0a616527a8cec7"),
+        pubkey: String::from("033e514ff30be0ea421f9512da0ed1aea52ea541275654d034bde3470a61269285"),
         tlv_records: tlvs,
         ..Default::default()
     };
